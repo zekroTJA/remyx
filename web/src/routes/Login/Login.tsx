d76d16c06ea141fafaf0c2547bb1024e23962583
zekroTJA <riho@live.de>
2023-04-02 16:08:02 +0000

    add invite only warning

--- web/src/routes/Login/Login.tsx
+++ web/src/routes/Login/Login.tsx
@@ -1,5 +1,6 @@
 import { Component } from "solid-js";
 import Spotify from "../../assets/spotify";
+import Warning from "../../assets/warning";
 import { loginUrl } from "../../services/api/client";
 import styles from "./Login.module.scss";
 
@@ -8,6 +9,16 @@ export const Login: Component = () => {
     <div class={styles.container}>
       <div class={styles.login}>
         <h1>REMYX</h1>
+        <div class="mb2 card warning" style={{ "margin-bottom": "1.8em" }}>
+          <div class="heading">
+            <Warning />
+            <strong>Warning</strong>
+          </div>
+          Because the Spotify API application is currently in the process of
+          being reviewed, the login to the app is invite-only at the moment.
+          Feel free to <a href="https://www.zekro.de/contact">contact</a> me so
+          I can add you to the testers list.
+        </div>
         <a class="button" href={loginUrl()}>
           <Spotify />
           Login with Spotify
